3758f8910840e7fe1b2d96c96e3c2ae8ad862861
Laura Trotta <laura.trotta@elastic.co>
2024-05-09 15:04:41 +0200

    version from property file

--- java-client-serverless/build.gradle.kts
+++ java-client-serverless/build.gradle.kts
@@ -34,7 +34,7 @@ plugins {
 }
 
 // GitHub Maven repo doesn't like 1.0.0+20231031-SNAPSHOT
-version = "1.0.0-20231031-SNAPSHOT"
+version = (File(project.rootDir, "config/version-serverless.txt").readText().trim() + "-SNAPSHOT")
 
 signing {
     sign(publishing.publications)
